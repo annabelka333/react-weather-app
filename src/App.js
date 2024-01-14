bd7e5b494af08be8de5c303dc29ed85846dbda74
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-01-14 22:15:58 +0200

    Added GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a href="https://www.instagram.com/vediashkyna/">
+          {" "}
+          Anna Vediashkyna
+        </a>{" "}
+        and it is{" "}
+        <a href="https://prismatic-trifle-d81140.netlify.app/" target="blank">
+          opened-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
