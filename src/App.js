4e196be51bbe4f406a7c32f68ecc89ad88f08f2c
annabelka333 <139398794+annabelka333@users.noreply.github.com>
2024-01-15 22:24:27 +0200

    init install

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a href="https://www.instagram.com/vediashkyna/">
-          {" "}
-          Anna Vediashkyna
-        </a>{" "}
-        and it is{" "}
-        <a href="https://prismatic-trifle-d81140.netlify.app/" target="blank">
-          opened-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          <button btn btn-primary>
+            Hello
+          </button>
+          This project was coded by
+          <a href="https://www.instagram.com/vediashkyna/" target="blank">
+            {" "}
+            Anna Vediashkyna
+          </a>{" "}
+          and it is{" "}
+          <a href="https://prismatic-trifle-d81140.netlify.app/" target="blank">
+            opened-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
